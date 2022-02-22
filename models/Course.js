7fbef7fb25e9d2917d051c672ad5997986f705c9
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-22 11:43:06 +0200

    add course model

---
+++ models/Course.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const pattern  = /^[A-Za-z0-9]{5,}$/;
+
+const userSchema = new mongoose.Schema({
+    title         : {type: String, required: true},
+    description   : {type: String, required: true},
+    image         : {type: String, required: true},
+    isPublic      : {type: Boolean, default: false },
+    enrolledUsers : [{type: mongoose.Types.ObjectId, ref: 'User'}],
+},{
+    
+    timestamps: true
+});
+
+const Course = mongoose.model('Course', userSchema);
+
+module.exports = Course;
