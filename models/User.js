73f8ef9ce56d16ad5f96847c3a4475fac601a5ce
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-22 08:41:18 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, }
+});
+
+userSchema.index({ username: 1 }, {
+    unique: true,
+    collation: {
+        locale   : 'en',
+        strength : 2
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
