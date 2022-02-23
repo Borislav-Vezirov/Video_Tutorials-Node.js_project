f490ea5c918f6b53775b9a3692255e5cd4c61afa
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-23 10:25:31 +0200

    added details page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,17 +1,51 @@
-const { getAllCourse } = require('../services/courseServices.js');
+const { isUser } = require('../middlewares/guards.js');
+const { getAllCourse, getOneById } = require('../services/courseServices.js');
 
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
 
-    let posts =  await getAllCourse();
+    let allPosts = await getAllCourse();
+
+    let posts = [];
+    
+    allPosts.map(x => x.isPublic == true ? posts.push(x) : null );
 
     const datePattern = new RegExp(/^[A-Za-z]+ | [\d+:\d+:\d+]+ GMT/gi)
     
-    posts.forEach(x => x.createdAt = x.createdAt.toString().split(datePattern)[1]);
-    console.log(posts);
+    posts.map(x => x.createdAt = x.createdAt.toString().split(datePattern)[1]);
 
     res.render('home', { pageTitle: 'Home Page', posts });
+});
+
+router.get('/details/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    const post = await getOneById(id);
+    
+    if(req.session.user){
+        
+        post.hasUser = true;
+        
+        if(req.session.user._id == post.author._id){
+            
+            post.isAuthor = true;
+        }else{
+            
+            post.isJoined = post.enrolledUsers.find(x => x._id == req.session.user._id) != undefined;
+            
+        };
+    };
+
+    res.render('details', { title: 'Details Page', ...post });
+});
+
+router.get('/profile', isUser(), async (req, res) => {
+    
+    const user = req.session.user;
+    
+    res.render('profile', { title: 'Profile Page' , user});
 });
 
 
--- services/courseServices.js
+++ services/courseServices.js
@@ -1,5 +1,5 @@
 const Course = require("../models/Course.js");
-
+const User   = require("../models/User.js");
 
 async function createPost(course){
 
@@ -15,7 +15,50 @@ async function getAllCourse(){
     return await Course.find({}).lean();
 };
 
+async function getOneById(id){
+
+    return await Course.findById(id).lean();
+};
+
+async function getOneAndUpdate(id, post){
+
+    return await Course.findByIdAndUpdate(id, post);
+
+};
+
+async function deletePost(id){
+
+    return await Course.findOneAndDelete({_id: id});
+};
+
+
+async function enroll(postId, userId){
+
+    const post = await Course.findById(postId);
+    
+    if(post && post.enrolledUsers.includes(userId)){
+        throw new Error('You already enrolled!')
+    };
+
+    const user = await User.findById(userId);
+
+    console.log(user);
+
+    user.courses.push(postId);
+
+    post.enrolledUsers.push(userId);
+
+    await post.save();
+};
+
+
+
+
 module.exports = {
     createPost,
-    getAllCourse
+    getAllCourse,
+    getOneById,
+    getOneAndUpdate,
+    deletePost,
+    enroll
 }
